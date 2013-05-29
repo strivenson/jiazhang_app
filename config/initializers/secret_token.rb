b1a2f2ae4f26f6b2bd33b140eccc4e1ab367d7ed
jiazhang <strivenson@gmail.com>
2013-05-29 17:39:12 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JiazhangApp::Application.config.secret_token = 'e1306ed5a3d8a59df715c8e8267ee065323673f5521e32541fd1d01dd26b9dd25ef752bbc614b48a2ff2580d973dcf20282db417bc5f0d493e33efa4f99310e7'
